b4f88ff3a7037e872540851f13158ac36d5a79b7
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-09-28 16:18:15 -0500

    Implements a button with a reset feature

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ for (i = 0; i < userInput; i++) {
 }
 
 const blocks = document.getElementsByClassName('block');
-Array.from(blocks).forEach((block) => {
+const blocksArray = Array.from(blocks);
+
+blocksArray.forEach((block) => {
     block.addEventListener('mouseover', () => {
-        block.classList.toggle('hover');
-    })
-})
+        if (block.classList.contains('hover')) {
+            return;
+        } else {
+            block.classList.toggle('hover');
+        }
+    });
+});
 
-
-/*
-add hover event listener
-toggle a class .hover
-
-*/
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    blocksArray.forEach((block) => {
+        block.classList.remove('hover');
+    });   
+});
