3e6446a4ac0ea882122a861b691b64f863b0992b
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-10-03 16:27:24 -0500

    Solves issue with adding event listener when reset

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
 const grid = document.querySelector('#grid');
-
-const userInput = 110 //temporary
-
-for (i = 0; i<userInput; i++) {
-    const row = document.createElement('div');
-    row.className='row';
-    
-    for (j = 0; j < userInput; j++) {
-        const block = document.createElement('div');
-        block.className = 'block';
-        row.appendChild(block);   
-    }
-    grid.appendChild(row);
-}
-
+const resetButton = document.querySelector('#reset');
 const blocks = document.getElementsByClassName('block');
 const blocksArray = Array.from(blocks);
+const sizeButton = document.querySelector('#prompt');
+const rows = document.getElementsByClassName('row');
+const rowsArray = Array.from(rows);
 
-blocksArray.forEach((block) => {
-    block.addEventListener('mouseover', () => {
-        if (block.classList.contains('hover')) {
-            return;
-        } else {
-            block.classList.toggle('hover');
+
+
+
+
+
+sizeButton.addEventListener('click', () => {
+    return createGrid(changeSize());
+})
+
+function createGrid(userInput) {
+    reset();
+
+    for (i = 0; i < userInput; i++) {
+        const row = document.createElement('div');
+        row.className='row';
+        
+        for (j = 0; j < userInput; j++) {
+            const block = document.createElement('div');
+            block.className = 'block';
+            row.appendChild(block);   
+            block.addEventListener('mouseover', () => {
+                if (!block.classList.contains('hover')) {
+                    block.classList.toggle('hover');
+                }
+            });
         }
-    });
-});
+        grid.appendChild(row);   
+    }
+}
 
-const resetButton = document.querySelector('#reset');
-resetButton.addEventListener('click', () => {
-    blocksArray.forEach((block) => {
-        block.classList.remove('hover');
-    });   
-});
+    // delegate event listener to #grid
+    // stuck!!!
+
+function changeSize() {
+    const userInput = parseInt(prompt("Change grid size", "Example: 16"));
+    if (Number.isNaN(userInput)) {
+        return changeSize();
+    } else {
+        return userInput;
+    }
+}
+
+function reset() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+resetButton.addEventListener('click', () => reset());
